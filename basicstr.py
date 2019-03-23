0d70dae2a4ab7ed92d250279dbdc10ffdc88b9a2
liberbell <liberbell@gmail.com>
2019-03-23 15:53:15 +0900

    basicstr3

--- basicstr.py
+++ basicstr.py
@@ -1,1 +1,10 @@
 import string
+
+print(string.ascii_letters)
+print(string.digits)
+print(string.hexdigits)
+print(string.punctuation)
+
+test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of December"
+test_string2 = "Supercalifraglistic"
+test_string3 = "90210"
