0fe2d6808ded9f4bb07f7f77fc2dd26f07e77a7a
liberbell <liberbell@gmail.com>
2019-03-23 15:56:52 +0900

    basicstr4

--- basicstr.py
+++ basicstr.py
@@ -8,3 +8,5 @@ print(string.punctuation)
 test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of December"
 test_string2 = "Supercalifraglistic"
 test_string3 = "90210"
+
+result = ".join([c for c in test_string1 if c in string.ascii_letters])
