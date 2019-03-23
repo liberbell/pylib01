7219cb793447375c27fd6b92b28b1d25e98ed563
liberbell <liberbell@gmail.com>
2019-03-23 16:10:31 +0900

    search_str4

--- search_str.py
+++ search_str.py
@@ -3,3 +3,6 @@ sampleStr = "The quick brown fox jumps over the lazy dog"
 print(sampleStr.startswith("The"))
 print(sampleStr.startswith("the"))
 print(sampleStr.endswith("dog"))
+
+print(sampleStr.find("the"))
+print(sampleStr.rfind("the"))
