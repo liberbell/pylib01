79c23659d9db6d65f60311316062c433e5b04d10
liberbell <liberbell@gmail.com>
2019-03-23 15:58:01 +0900

    basicstr6

--- basicstr.py
+++ basicstr.py
@@ -9,4 +9,5 @@ test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of Decemb
 test_string2 = "Supercalifraglistic"
 test_string3 = "90210"
 
-result = ".join([c for c in test_string1 if c in string.ascii_letters])
+result = "".join([c for c in test_string1 if c in string.ascii_letters])
+print(result)
