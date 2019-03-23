95636b1ecf6968cc15456810a2ffc748b48efd2f
liberbell <liberbell@gmail.com>
2019-03-23 15:57:07 +0900

    basicstr5

--- basicstr.py
+++ basicstr.py
@@ -1,9 +1,9 @@
 import string
 
-print(string.ascii_letters)
-print(string.digits)
-print(string.hexdigits)
-print(string.punctuation)
+# print(string.ascii_letters)
+# print(string.digits)
+# print(string.hexdigits)
+# print(string.punctuation)
 
 test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of December"
 test_string2 = "Supercalifraglistic"
