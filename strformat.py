d62c3de3834cba2e192d739c54eac72f10af4045
liberbell <liberbell@gmail.com>
2019-03-24 14:36:39 +0900

    strformat5

--- strformat.py
+++ strformat.py
@@ -4,6 +4,10 @@ the_str = "The quick brown fox jumps over the lazy dog"
 the_templage = Template(the_str)
 output_str = the_templage.substitute(animal="fox", action="jumped")
 print(output_str)
+args = {
+"animal": "cow",
+"action": "walked"
+}
 
 foo = "foo"
 bar = 123
