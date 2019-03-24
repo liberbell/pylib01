7f755c4b53d3fc62cc270ebd8152230b49c8ca7c
liberbell <liberbell@gmail.com>
2019-03-24 14:14:55 +0900

    strmanip4

--- strmanip.py
+++ strmanip.py
@@ -2,6 +2,9 @@
 
 test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of December"
 
+print(test_string1.upper())
+print(test_string1.lower())
+
 names = ["Amy", "John", "George", "Michels", "Penelope"]
 biggest = max(len(name) for name in names)
 
