983a3e09dbb0ce8149058c59ecc06bb2dec16841
liberbell <liberbell@gmail.com>
2019-03-25 07:57:51 +0900

    sorting4

--- sorting.py
+++ sorting.py
@@ -5,5 +5,5 @@ testScores = [82, 67, 94, 73, 79, 86, 58, 91, 89, 71, 88, 77, 84]
 sortedScores = sorted(testScores)
 print(sortedScores)
 
-sortedScores = sorted(testScores, reverse=true)
+sortedScores = sorted(testScores, reverse=True)
 print(sortedScores)
