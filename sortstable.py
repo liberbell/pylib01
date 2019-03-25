193cb2cd714cbb4846b439c7d461ab1ed11a4c69
liberbell <liberbell@gmail.com>
2019-03-26 07:55:46 +0900

    sortstable2

---
+++ sortstable.py
@@ -0,0 +1,24 @@
+
+
+class Product():
+    def __init__(self, name, price, weight, discount):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.discount = discount
+
+    def __repr__(self):
+        return repr((self.name, self.price, self.weight))
+
+    def discountPrice(self):
+        return self.price - (self.price * self.discount)
+
+prodList = [
+Product("Dooickey", 40, 8, 0.15),
+Product("Widget", 50, 10, 0.05),
+Product("Dooickey", 40, 8, 0.15),
+Product("Thingamabob", 35, 12, 0.0),
+Product("Gadget", 65, 7, 0.20)
+]
+
+print(sorted(prodList, key=lambda p:p.price))
