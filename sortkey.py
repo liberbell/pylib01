172ad734f9e740197aa03c9d436f00e8ee2572a9
liberbell <liberbell@gmail.com>
2019-03-26 07:40:50 +0900

    sortkey1

---
+++ sortkey.py
@@ -0,0 +1,14 @@
+
+
+class Product():
+    def __init__(self, name, price, weight, discount):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.discount = discount
+
+    def __repr__(self):
+        return repr((self.name, self.price, self.weight))
+
+    def discountPrice(self):
+        return self.price - (self.price * self.discount)
