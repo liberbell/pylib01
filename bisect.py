39876c35a48fe95ee99d9aa5207af834ba418c22
liberbell <liberbell@gmail.com>
2019-03-29 07:49:35 +0900

    bisect4

--- bisect.py
+++ bisect.py
@@ -3,5 +3,11 @@ import bisect
 values = [5, 7, 13, 20, 25, 31, 36, 43, 47, 49, 50, 75]
 
 breakpoint = [60, 70, 80, 90]
-gradeletters = 'FDCBA'
+gradeLetters = 'FDCBA'
 scores = [81, 68, 53, 91, 90, 82, 76, 71, 84]
+
+def calsGrade(score):
+    i = 0
+    return gradeLetters[i]
+
+results = [calsGrade(score=) for score in scores]
