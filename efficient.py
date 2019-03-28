67132a11ac9372d39a17a344be324b73deb2ad05
liberbell <liberbell@gmail.com>
2019-03-28 16:31:21 +0900

    efficient4

--- efficient.py
+++ efficient.py
@@ -4,4 +4,9 @@ arr1 = array('i', [2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
 print(arr1.typecode)
 print("Array 1 item size is: ", arr1.itemsize)
 
-arr2 = array('b', [18, 102, 182, 56, 89, 5, 254, 32, 64, 50])
+arr1.insert(0,0)
+arr1.append(22)
+arr1.extend([24, 26, 28])
+print(arr1)
+
+# arr2 = array('b', [18, 102, 182, 56, 89, 5, 254, 32, 64, 50])
