e0ded326cf385f9b4630781316fc413afa6f2f5c
liberbell <liberbell@gmail.com>
2019-03-29 07:51:12 +0900

    bisect5

--- bisect.py
+++ bisect.py
@@ -2,6 +2,10 @@ import bisect
 
 values = [5, 7, 13, 20, 25, 31, 36, 43, 47, 49, 50, 75]
 
+print(bisect.bisect(values, 25))
+print(bisect.bisect_right(values, 25))
+print(bisect.bisect_left(values, 25))
+
 breakpoint = [60, 70, 80, 90]
 gradeLetters = 'FDCBA'
 scores = [81, 68, 53, 91, 90, 82, 76, 71, 84]
