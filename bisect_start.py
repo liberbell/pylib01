e29bdcbcb508b088ea334dfe9e6be0f67fed6308
liberbell <liberbell@gmail.com>
2019-03-30 13:59:50 +0900

    bisect1

--- bisect_start.py
+++ bisect_start.py
@@ -2,20 +2,21 @@ import bisect
 
 values = [5, 7, 13, 20, 25, 31, 36, 43, 47, 49, 50, 75]
 
-print(bisect.bisect(values, 25))
-print(bisect.bisect_right(values, 25))
-print(bisect.bisect_left(values, 25))
+# print(bisect.bisect(values, 25))
+# print(bisect.bisect_right(values, 25))
+# print(bisect.bisect_left(values, 25))
 
-print(values)
+# print(values)
 bisect.insort_right(values, 25)
-print(values)
+# print(values)
 
 breakpoint = [60, 70, 80, 90]
 gradeLetters = 'FDCBA'
 scores = [81, 68, 53, 91, 90, 82, 76, 71, 84]
 
 def calsGrade(score):
-    i = 0
+    # i = 0
+    i = bisect.bisect(breakpoints, score)
     return gradeLetters[i]
 
 results = [calsGrade(score) for score in scores]
