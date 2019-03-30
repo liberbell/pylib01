6a185127bd8e5798212fbb7a16d4eecce22a05c7
liberbell <liberbell@gmail.com>
2019-03-30 14:23:08 +0900

    tempdfiles4

--- tempfiles.py
+++ tempfiles.py
@@ -3,3 +3,7 @@ import tempfile
 
 print("gettempdir: ", tempfile.gettempdir())
 print("gettempprefix: ", tempfile.gettempprefix())
+
+(tempfh, tempfp) = tempfile.mkstemp(".tmp","testtemp",None,True)
+f = os.fdopen(tempfh,"w+t")
+f.write("This is some temp data")
