5a49cb61ad6eabd4fe234bd854065959d42f5706
liberbell <liberbell@gmail.com>
2019-03-30 14:16:00 +0900

    filebasic7

--- filebasic.py
+++ filebasic.py
@@ -1,15 +1,16 @@
 
 
-fp = open("testfile.txt", "w")
-fp.write("This is some text\n")
-fp.close()
+# fp = open("testfile.txt", "w")
+# fp.write("This is some text\n")
+# fp.close()
 
 with open("testfile.txt", "r") as fp:
     data = fp.read()
     print(data)
 
-with open("testfile.txt", "a") as fp:
+with open("testfile.txt", "a+") as fp:
     fp.write("This is data added onto the file\n")
+    fp.seek(0)
 
 with open("testfile.txt", "r") as fp:
     data = fp.read()
