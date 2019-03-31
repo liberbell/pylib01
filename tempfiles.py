1ee3ed0ecc75a28845b93c6544c6220bddd65796
liberbell <liberbell@gmail.com>
2019-03-31 16:10:15 +0900

    tempfiles2

--- tempfiles.py
+++ tempfiles.py
@@ -4,10 +4,15 @@ import tempfile
 print("gettempdir: ", tempfile.gettempdir())
 print("gettempprefix: ", tempfile.gettempprefix())
 
-(tempfh, tempfp) = tempfile.mkstemp(".tmp","testtemp",None,True)
-f = os.fdopen(tempfh,"w+t")
-f.write("This is some temp data")
-f.seek(0)
-print(f.read())
-f.close()
-os.remove(tempfp)
+# (tempfh, tempfp) = tempfile.mkstemp(".tmp","testtemp",None,True)
+# f = os.fdopen(tempfh,"w+t")
+# f.write("This is some temp data")
+# f.seek(0)
+# print(f.read())
+# f.close()
+# os.remove(tempfp)
+
+with tempfile.TemporaryFile(mode="w+t") as tfp:
+    tfp.write("This is some temp data")
+    tfp.seek(0)
+    print(tfp.read())
