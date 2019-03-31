f6944bb97d41ea066f15cdfa8be1024ed04dff7e
liberbell <liberbell@gmail.com>
2019-04-01 07:58:18 +0900

    zipfile5

--- zipfile.py
+++ zipfile.py
@@ -1,6 +1,6 @@
 import zipfile
 
-zfile = zipfile.ZipFile("archive.zip","w")
+zfile = zipfile.zipfile("archive.zip","w")
 zfile.write("file1.txt")
 zfile.write("file2.txt")
 zfile.write("file3.txt")
