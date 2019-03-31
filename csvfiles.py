e8ef881524727be853ff39d5aa7eda0c142c2d4e
liberbell <liberbell@gmail.com>
2019-03-31 16:24:50 +0900

    csvfiles4

--- csvfiles.py
+++ csvfiles.py
@@ -4,10 +4,12 @@ def readerSample():
     with open("StockQuotes.csv") as datafile:
         reader = csv.reader(datafile)
         for row in reader:
-            print(row) 
+            print(row)
 
 def useSniffer():
     pass
 
 def writeSample():
     pass
+
+readerSample()
