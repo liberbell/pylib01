5e7b0598afc337db8a462196b811d5f953da562f
liberbell <liberbell@gmail.com>
2019-04-02 07:48:19 +0900

    config_start3

--- config_start.py
+++ config_start.py
@@ -4,5 +4,8 @@ parser = configparser.ConfigParser()
 
 parser.read("config.cfg")
 
-print(parser.sections())
-print(parser.has_section("Section 1"))
+# print(parser.sections())
+# print(parser.has_section("Section 1"))
+
+using_time_travel = parser['DEFAULT']['USETimeTravel']
+print(using_time_travel)
