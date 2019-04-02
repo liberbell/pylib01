41b22cd88afafb34612649050c26635a080d591d
liberbell <liberbell@gmail.com>
2019-04-03 07:51:41 +0900

    randam_start7

--- randam_start.py
+++ randam_start.py
@@ -10,6 +10,16 @@ import random
 #     else:
 #         print("Tails")
 
-print(random.uniform(1,100))
-print(random.randint(1,100))
-print(random.randrange(0,101,5))
+# print(random.uniform(1,100))
+# print(random.randint(1,100))
+# print(random.randrange(0,101,5))
+
+random.seed(1)
+print(random.random())
+print(random.random())
+
+print("-------------")
+
+random.seed(1)
+print(random.random())
+print(random.random())
