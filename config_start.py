6caad61bd1cc55375d7008f96a5fd00ea74850b9
liberbell <liberbell@gmail.com>
2019-04-03 07:38:29 +0900

    config_start3

--- config_start.py
+++ config_start.py
@@ -8,14 +8,17 @@ parser.read("config.cfg")
 # print(parser.has_section("Section 1"))
 
 using_time_travel = parser['DEFAULT']['USETimeTravel']
-print(using_time_travel)
-print(type(using_time_travel))
+# print(using_time_travel)
+# print(type(using_time_travel))
+#
+# using_time_travel = bool(parser['DEFAULT']['USETimeTravel'])
+# print(using_time_travel)
+# print(type(using_time_travel))
+#
+# opd = parser['DEFAULT'].getboolean('ObeyPrimeDirective')
+# print(opd)
+# speed = parser['DEFAULT'].getfloat('Ship Speed')
+# print(speed)
 
-using_time_travel = bool(parser['DEFAULT']['USETimeTravel'])
-print(using_time_travel)
-print(type(using_time_travel))
-
-opd = parser['DEFAULT'].getboolean('ObeyPrimeDirective')
-print(opd)
-speed = parser['DEFAULT'].getfloat('Ship Speed')
-print(speed)
+title = parser['Section 3']['Title']
+print(title)
