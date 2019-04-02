efe71211a768357fba22267296c3d38b7f94956b
liberbell <liberbell@gmail.com>
2019-04-03 07:46:11 +0900

    randam_start4

--- randam_start.py
+++ randam_start.py
@@ -1,5 +1,13 @@
 import random
 
-print(random.random())
-print(random.random())
-print(random.random())
+# print(random.random())
+# print(random.random())
+# print(random.random())
+
+for i in range(10):
+    if (random.random() <= 0.5):
+        print(i)
+        print("Heads")
+    else:
+        print(i)
+        print("Tails")
