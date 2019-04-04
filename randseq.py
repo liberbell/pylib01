b0290e1829c1de865add6a6c4954d5b59d14add6
liberbell <liberbell@gmail.com>
2019-04-04 21:35:48 +0900

    randseq4

--- randseq.py
+++ randseq.py
@@ -5,6 +5,7 @@ moves = ["rock", "paper", "scissors"]
 # print(random.choice(moves))
 
 roulette_wheel = ["black", "red", "green"]
-print(random.choices(roulette_wheel, k=10))
+weight = [18,18,2]
+print(random.choices(roulette_wheel, weight, k=10))
 
 players = ["Bill", "Jane", "Joe", "Sally", "Mike", "Lynsay"]
