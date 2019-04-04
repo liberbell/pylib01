18561f9583bdae9720b8184dd9c59612a8550566
liberbell <liberbell@gmail.com>
2019-04-04 21:33:44 +0900

    randseq3

--- randseq.py
+++ randseq.py
@@ -2,8 +2,9 @@ import random
 import string
 
 moves = ["rock", "paper", "scissors"]
-print(random.choice(moves))
+# print(random.choice(moves))
 
 roulette_wheel = ["black", "red", "green"]
+print(random.choices(roulette_wheel, k=10))
 
 players = ["Bill", "Jane", "Joe", "Sally", "Mike", "Lynsay"]
