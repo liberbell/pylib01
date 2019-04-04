fb6901524a36a428c14c9d41e8f1a5621fe8f670
liberbell <liberbell@gmail.com>
2019-04-05 07:53:40 +0900

    uuid_start6

--- uuid_start.py
+++ uuid_start.py
@@ -7,5 +7,10 @@ print(result.hex)
 print(result.urn)
 print("~~~~~~~~~~~~~~~~~\n")
 
+result = uuid.uuid5(uuid.NAMESPACE_DNS,"example.com")
+print("UUID5: ", result)
+print(result)
+print(result.hex)
+print(result.urn)
 
 print("~~~~~~~~~~~~~~~~~\n")
