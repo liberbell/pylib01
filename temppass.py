4a27912d188bd1b944314debe562d64bb2b2ef70
liberbell <liberbell@gmail.com>
2019-04-05 07:36:49 +0900

    temppass5

--- temppass.py
+++ temppass.py
@@ -3,7 +3,7 @@ import string
 
 def genarateTempPass(numChars=8):
     potentialChars = string.ascii_letters + string.digits + "+=?/!@#%"
-    result = ''.join(choice(potentialChars) for i in range(numChars))
+    result = ''.join(secrets.choice(potentialChars) for i in range(numChars))
     return result
 
 def generateBetterPass(numChars=8):
