5144532c229fef0e062083cc6317f3dbd40b43a9
liberbell <liberbell@gmail.com>
2019-04-04 21:54:05 +0900

    randcryp3

--- randcryp.py
+++ randcryp.py
@@ -2,6 +2,7 @@ import os
 import secrets
 
 result = os.urandom(8)
-print([hex(b) for b in result])
+# print([hex(b) for b in result])
 
 moves = ["rock", "paper", "scissors"]
+print(secrets.choice(moves))
