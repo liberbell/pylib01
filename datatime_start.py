cebd048fa13832d319c87f260d2b6e043ec5bb30
liberbell <liberbell@gmail.com>
2019-04-06 15:28:04 +0900

    datatime_start4

--- datatime_start.py
+++ datatime_start.py
@@ -1,10 +1,17 @@
 from datetime import date, time, datetime
 
 d1 = date.today()
-print(d1)
+# print(d1)
 
 t1 = time(12, 30, 00)
-print(t1)
+# print(t1)
 
 dt1 = datetime.now()
-print(dt1)
+# print(dt1)
+
+
+print(d1.day)
+print(d1.year)
+
+print(t1.hour)
+print(dt1.month)
