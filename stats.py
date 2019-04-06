18ffe66ca7de25e507906fd5182ef9dec237d2c7
liberbell <liberbell@gmail.com>
2019-04-06 15:03:22 +0900

    stats4

--- stats.py
+++ stats.py
@@ -5,11 +5,14 @@ import array
 sample_data1 = [1, 3, 5, 7]
 sample_data2 = [2, 3, 5, 4, 3, 5, 3, 2, 5, 6, 4, 3]
 
-print(statistics.mean(sample_data1))
+# print(statistics.mean(sample_data1))
 
-print(statistics.median(sample_data1))
-print(statistics.median_low(sample_data1))
-print(statistics.median_high(sample_data1))
+# print(statistics.median(sample_data1))
+# print(statistics.median_low(sample_data1))
+# print(statistics.median_high(sample_data1))
+
+print(statistics.mode(sample_data2))
+
 
 def readData():
     with open("StockQuotes.csv") as dataFile:
