b38bb3a7fae2489c5410720820924babaf12db4d
liberbell <liberbell@gmail.com>
2019-04-06 14:59:34 +0900

    stats2

--- stats.py
+++ stats.py
@@ -5,9 +5,11 @@ import array
 sample_data1 = [1, 3, 5, 7]
 sample_data2 = [2, 3, 5, 4, 3, 5, 3, 2, 5, 6, 4, 3]
 
-
+print(statistics.mean(sample_data1))
 
 
 def readData():
     with open("StockQuotes.csv") as dataFile:
         data = array.array('f', [])
+
+        reader = csv.reader(dataFile)
