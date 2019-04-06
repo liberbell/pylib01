2a73a1ce80f30c09df05c4d3a34a809495c673bb
liberbell <liberbell@gmail.com>
2019-04-06 14:57:28 +0900

    stats1

--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 import statistics
 import csv
 import array
+
+sample_data1 = [1, 3, 5, 7]
+sample_data2 = [2, 3, 5, 4, 3, 5, 3, 2, 5, 6, 4, 3]
+
+
+
+
+def readData():
+    with open("StockQuotes.csv") as dataFile:
+        data = array.array('f', [])
