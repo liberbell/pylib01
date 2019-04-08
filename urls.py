be4315f3a6e261063ccda19953e6f277a09863d8
liberbell <liberbell@gmail.com>
2019-04-08 22:50:14 +0900

    urls3

--- urls.py
+++ urls.py
@@ -8,11 +8,14 @@ result = urllib.parse.urlparse(sample_url)
 # print(result.geturl())
 
 sample_string = "Hello El こんにちは"
-print(urllib.parse.quote(sample_string))
-print(urllib.parse.quote_plus(sample_string))
+# print(urllib.parse.quote(sample_string))
+# print(urllib.parse.quote_plus(sample_string))
 
 query_data = {
     "Name": "Jhon Doe",
     "City": "Anytown USA",
     "Age": 37
 }
+
+result = urllib.parse.urlencode(query_data)
+print(result)
