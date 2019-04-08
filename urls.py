7cd8bc9f79c5140be191f449a913ac38b167e2c2
liberbell <liberbell@gmail.com>
2019-04-08 12:09:42 +0900

    urls2

---
+++ urls.py
@@ -0,0 +1,11 @@
+import urllib.parse
+
+sample_url = "http://server.example.com:8080/example.html?val1=1&val2=Hello+World"
+
+sample_string = "Hello El Nino"
+
+query_data = {
+    "Name": "Jhon Doe",
+    "City": "Anytown USA",
+    "Age": 37
+}
