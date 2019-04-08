5d1aeb15f1f267848badd7adc52ba49e167a4605
liberbell <liberbell@gmail.com>
2019-04-08 22:47:25 +0900

    urls1

--- urls.py
+++ urls.py
@@ -3,11 +3,12 @@ import urllib.parse
 sample_url = "http://server.example.com:8080/example.html?val1=1&val2=Hello+World"
 
 result = urllib.parse.urlparse(sample_url)
-print(result)
-print(result.scheme, result.hostname, result.path)
-print(result.geturl())
+# print(result)
+# print(result.scheme, result.hostname, result.path)
+# print(result.geturl())
 
-sample_string = "Hello El Nino"
+sample_string = "Hello El こんにちは"
+print(urllib.parse.quote(sample_string))
 
 query_data = {
     "Name": "Jhon Doe",
