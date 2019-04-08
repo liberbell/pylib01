add48893f193021a4e1b955cd6b6011898c2a683
liberbell <liberbell@gmail.com>
2019-04-09 07:42:17 +0900

    html_start2

--- html_start.py
+++ html_start.py
@@ -1,3 +1,15 @@
 from html.parser import HTMLParser
 
- 
+
+class HTMLParser(HTMLParser):
+    def handler_starttag(self, tag, attrs):
+        pass
+
+    def handler_endtag(self, tag):
+        pass
+
+    def handler_data(self, data):
+        pass
+
+    def handler_comment(self, data):
+        pass
