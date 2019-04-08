a15e0c87a4cf5ade749a06b433c4d20dbcf8a8ed
liberbell <liberbell@gmail.com>
2019-04-08 12:11:13 +0900

    urls3

--- urls.py
+++ urls.py
@@ -2,6 +2,9 @@ import urllib.parse
 
 sample_url = "http://server.example.com:8080/example.html?val1=1&val2=Hello+World"
 
+result = urllib.parse.urlparse(sample_url)
+print(result)
+
 sample_string = "Hello El Nino"
 
 query_data = {
