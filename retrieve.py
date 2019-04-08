f44a989e81ab14458eb1802b1e2e460830c497b2
liberbell <liberbell@gmail.com>
2019-04-09 06:38:40 +0900

    retrieve1

--- retrieve.py
+++ retrieve.py
@@ -2,5 +2,11 @@ import urllib.request
 
 sample_url = "http://httpbin.org/xml"
 
+resp = urllib.request.urlopen(sample_url)
 
-status_code = 0
+status_code = resp.status
+print(status_code)
+
+
+if status >= 200 and status_code < 300:
+    pass
