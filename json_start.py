eaec4a7e62558521bc93f64db05d9d912d51b30e
liberbell <liberbell@gmail.com>
2019-04-10 08:01:02 +0900

    json_start3

--- json_start.py
+++ json_start.py
@@ -1,2 +1,11 @@
 import urllib.request
 import json
+
+
+objdata = {
+    "name": "Joe Marine",
+    "author": True,
+    "titles": [
+    "Leading Python", "Advanced Python", "Python Standard Library Essential Training"
+    ]
+}
