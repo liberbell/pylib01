ab1252018fa65ad6f5fc8088301ebbe726ef9b1a
liberbell <liberbell@gmail.com>
2019-04-11 07:59:32 +0900

    json_start1

--- json_start.py
+++ json_start.py
@@ -5,6 +5,9 @@ req = urllib.request.urlopen("http://httpbin.org/json")
 data = req.read().decode("utf-8")
 print(data)
 
+obj = json.loads(data)
+print(obj["slideshow"])
+
 objdata = {
     "name": "Joe Marine",
     "author": True,
